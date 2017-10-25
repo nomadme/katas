7c9236394d39dbb10c092dfd290b5c05d116103e
nomadme <nonomadme@gmail.com>
2017-10-25 10:41:31 -0500

    prime factor tdd

--- prime-factors/src/PrimeFactors.php
+++ prime-factors/src/PrimeFactors.php
@@ -1,8 +1,24 @@
 <?php
 
 class PrimeFactors {
-	
-	public function generate($number) {
-		return [];
+
+    /**
+     * @param $number
+     * @return array
+     */
+    public function generate($number)
+    {
+        $primes = [];
+
+        for ($candidate = 2; $number > 1; $candidate++)
+        {
+            for (; $number % $candidate == 0; $number /= $candidate)
+            {
+                $primes[] = $candidate;
+            }
+        }
+		
+		return $primes;
 	}
+	
 }
