7156ee86c2101889612c03124136bf263a65977c
nomadme <nonomadme@gmail.com>
2017-10-25 09:38:34 -0500

    initial setup

---
+++ prime-factors/spec/PrimeFactorsSpec.php
@@ -0,0 +1,13 @@
+<?php namespace spec;
+
+use PrimeFactors;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PrimeFactorsSpec extends ObjectBehavior {
+    
+    function it_returns_an_empty_array_for_1() {
+        $this->generate(1)->shouldReturn([]);
+    }
+    
+}
---
+++ prime-factors/src/PrimeFactors.php
@@ -0,0 +1,8 @@
+<?php
+
+class PrimeFactors {
+	
+	public function generate($number) {
+		return [];
+	}
+}
