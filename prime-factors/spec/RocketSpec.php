3b95ad98fc4db82800ebb7d00e5f59e1a6db2ed1
nomadme <nonomadme@gmail.com>
2017-10-26 09:29:04 -0500

    added rocket testing

---
+++ prime-factors/spec/RocketSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\Exception\Example\SkippingException;
+use Rocket;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RocketSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Rocket::class);
+    }
+
+    function it_flies_around_the_moon(){
+        if (!function_exists('rocketLaunch'))
+        {
+            throw new SkippingException(
+                'The rocket extension is not installed'
+            );
+        }
+
+        $this->flyToMoon();
+
+        return;
+    }
+}
---
+++ prime-factors/src/Rocket.php
@@ -0,0 +1,14 @@
+<?php
+
+class Rocket
+{
+    public function rocketLaunch()
+    {
+        return true;
+    }
+
+    public function flyToMoon()
+    {
+        return true;
+    }
+}
